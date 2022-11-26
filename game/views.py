e5daffba8a6bee83a32f4c52148ecf2261d34c27
Olly <bruja.meredith@pm.me>
2022-11-26 15:56:53 -0800

    fixed game retrieval on existing user. thinking about condensing cards together.

--- game/views.py
+++ game/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 
 from . import forms, table_logics, models
 
@@ -8,18 +9,22 @@ def index(request):
 
 def setup(request):
     if request.method == 'POST':
+        name = request.POST.get("player_name")
+        try:
+            table = models.Table.objects.get(player_name=name)
+            print(table.pk)
+            return redirect("bj:game", pk=table.pk)
+        except ObjectDoesNotExist:
+            pass
         partial_form = forms.TableForm(request.POST)
         if partial_form.is_valid():
-            # name = request.POST.get("player_name")
-            # table = get_object_or_404(models.Table, player_name=name)
-            # print(f"table found: {table}")
             table = partial_form.save(commit=False)
             game = table_logics.Game.create()
             game.save(table)
             return redirect("bj:game", pk=table.pk)
     else:
-        form = forms.TableForm()
-        return render(request, 'game/index.html', {'form': form})
+        index(request)
+
 
 def play_again(request, pk):
     if request.method == 'POST':
